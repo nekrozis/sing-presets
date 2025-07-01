bc530b782ae2836af8b09df42a45f2c1983501f0
necrosisy <mgal25@outlook.com>
2025-07-01 15:18:04 +0800

    feat(ss): implement Shadowsocks parser with base64 decoding, tag handling, and plugin support

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,12 +2,16 @@ import base64
 
 
 def try_decode_base64(content: str) -> str | None:
+    content = content.strip()
+    padding = len(content) % 4
+    if padding != 0:
+        content += "=" * (4 - padding)
+
     try:
-        padding = 4 - (len(content) % 4)
-        if padding != 4:
-            content += "=" * padding
-        decoded_bytes = base64.b64decode(content, validate=True)
-        decoded_str = decoded_bytes.decode("utf-8")
-        return decoded_str
+        if "-" in content or "_" in content:
+            decoded_bytes = base64.urlsafe_b64decode(content)
+        else:
+            decoded_bytes = base64.b64decode(content, validate=True)
+        return decoded_bytes.decode("utf-8")
     except Exception:
         return None
