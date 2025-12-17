d3b56d7e86cfabc48200016b9c13363a776b81f7
nekrozis <mgal25@outlook.com>
2025-12-17 11:46:56 +0800

    macro process

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,48 @@
 from transform import *
 import json
-
-cfgs = generate_configs("../subscription.txt")
+import yaml
 
 if __name__ == "__main__":
-    print(",".join(json.dumps(cfg.to_singbox_config()) for cfg in cfgs))
+    # 1. Generate configs
+    cfgs = generate_configs("../subscription.txt")
 
-    print("=" * 50)
+    proxy_nodes = [cfg.to_singbox_config() for cfg in cfgs]
+    proxy_tags = [cfg.to_singbox_config()["tag"] for cfg in cfgs]
 
-    print(",".join(json.dumps(cfg.to_singbox_config()["tag"]) for cfg in cfgs))
+    # 2. Load base template
+    with open("../base.yaml", "r", encoding="utf-8") as f:
+        base_yml = yaml.safe_load(f)
+
+    macros = {
+        "_PROXY_NODES_": proxy_nodes,
+        "_PROXY_TAGS_": proxy_tags,
+    }
+
+    def replace_macros(node):
+        if isinstance(node, list):
+            new_list = []
+            for item in node:
+                replaced = replace_macros(item)
+                if (
+                    isinstance(item, str)
+                    and item in macros
+                    and isinstance(replaced, list)
+                ):
+                    new_list.extend(replaced)
+                else:
+                    new_list.append(replaced)
+            return new_list
+        elif isinstance(node, dict):
+            return {k: replace_macros(v) for k, v in node.items()}
+        elif isinstance(node, str):
+            if node in macros:
+                return macros[node]
+            return node
+        else:
+            return node
+
+    cfg = replace_macros(base_yml)
+    cfg = json.dumps(cfg, ensure_ascii=False, indent=2)
+    print(cfg)
+    with open("../config.json", "w", encoding="utf-8") as f:
+        json.dump(cfg, f, ensure_ascii=False, indent=2)
